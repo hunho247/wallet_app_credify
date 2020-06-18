7c4eb8dc83ab948db004cd834746379f2496dabd
Thái Hưng <varmyapp@gmail.com>
2020-06-18 21:13:21 +0700

    initial

---
+++ app/src/main/java/one/credify/wallet_app_credify/MainActivity.java
@@ -0,0 +1,14 @@
+package one.credify.wallet_app_credify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
